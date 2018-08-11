144d24dac61325d6c3b57d21a19703ba6e99f1b0
Casper Shih <casshih@gmail.com>
2018-08-11 10:36:53 -0700

    updated

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var db = require("./models");
 
+//update
 // Setting express server and can be used in Heroku
 var app = express();
 var PORT = process.env.PORT || 3000;
