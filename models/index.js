48ea7f0180ade6c8562a33744eec0a90b75aa1f0
kevin8pizza <35049981+kevin8pizza@users.noreply.github.com>
2018-08-12 22:23:44 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -15,7 +15,10 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    config, {
+      host: "127.0.0.1",
+    dialect: 'mysql'
+  });
   );
 }
 
